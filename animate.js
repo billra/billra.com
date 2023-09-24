8f2e7c6308a45656f3e4acae84cb23ae5b618a13
Bill Ola Rasmussen <billra@gmail.com>
2023-09-24 08:54:45 +0200

    modernize code

--- animate.js
+++ animate.js
@@ -1,16 +1,17 @@
 // animate.js
 
-var draw_corners = false;
-var wrap_edge = false; // wrap or bounce
+let draw_corners = false;
+let wrap_edge = false; // wrap or bounce
 
-var erase_canvas = true;
-var erase_canvas_once; // after resize or other change
+let erase_canvas = true;
+let erase_canvas_once; // after resize or other change
 
-var canvas;
-
-var updateCount = 0;
-var count = 1000;
-var balls;
+let canvas;
+let ctx;
+let request;
+let balls;
+let updateCount = 0;
+let count = 1000;
 
 function rnd_velocity() {
     return (.5 + Math.random() * 3) * (Math.round(Math.random()) * 2 - 1);
@@ -18,8 +19,8 @@ function rnd_velocity() {
 
 function populate() {
     balls = [];
-    for (var i = 0; i < count; ++i) {
-        var r = 5 + Math.random() * 10;
+    for (let i = 0; i < count; ++i) {
+        const r = 5 + Math.random() * 10;
         balls.push({
             r: r,
             x: r + Math.random() * (canvas.width - 2 * r),
@@ -31,8 +32,6 @@ function populate() {
     }
 }
 
-var ctx;
-var request;
 function cancelAnimation() {
     if (!request) {
         return false;
@@ -50,30 +49,27 @@ function animate() {
     }
     erase_canvas_once = false;
 
-    for (var i = 0; i < balls.length; i++) {
+    for (let i = 0; i < balls.length; i++) {
         // draw
-        ctx.fillStyle = 'hsl(' + balls[i].hue++ + ', 100%, 60%)';
+        ctx.fillStyle = `hsl(${balls[i].hue++}, 100%, 60%)`;
         ctx.beginPath();
         ctx.arc(balls[i].x, balls[i].y, balls[i].r, 0, Math.PI * 2, true);
-        ctx.fill()
+        ctx.fill();
 
-        balls[i].x += balls[i].vx
-        balls[i].y += balls[i].vy
+        balls[i].x += balls[i].vx;
+        balls[i].y += balls[i].vy;
 
         if (balls[i].x - balls[i].r < 0) {
             if (wrap_edge) {
                 balls[i].x = canvas.width - 1 - balls[i].r; // todo: this 'sticks' value to edge, may want 'fold' value across edge
-            }
-            else {
+            } else {
                 balls[i].x = balls[i].r; // todo: this 'sticks' value to edge, may want 'fold' value across edge
                 balls[i].vx *= -1; // reverse direction, 'bounce'
             }
-        }
-        else if (balls[i].x + balls[i].r > canvas.width - 1) {
+        } else if (balls[i].x + balls[i].r > canvas.width - 1) {
             if (wrap_edge) {
                 balls[i].x = balls[i].r;
-            }
-            else {
+            } else {
                 balls[i].x = canvas.width - 1 - balls[i].r;
                 balls[i].vx *= -1;
             }
@@ -81,17 +77,14 @@ function animate() {
         if (balls[i].y - balls[i].r < 0) {
             if (wrap_edge) {
                 balls[i].y = canvas.height - 1 - balls[i].r;
-            }
-            else {
+            } else {
                 balls[i].y = balls[i].r;
                 balls[i].vy *= -1;
             }
-        }
-        else if (balls[i].y + balls[i].r > canvas.height - 1) {
+        } else if (balls[i].y + balls[i].r > canvas.height - 1) {
             if (wrap_edge) {
                 balls[i].y = balls[i].r;
-            }
-            else {
+            } else {
                 balls[i].y = canvas.height - 1 - balls[i].r;
                 balls[i].vy *= -1;
             }
@@ -110,27 +103,22 @@ function animate() {
 
 window.addEventListener("keydown", function (event) {
     console.log(event.key);
-    if ('k' == event.key) { // draw corner markers (debugging canvas size)
+    if (event.key === 'k') { // draw corner markers (debugging canvas size)
         draw_corners = !draw_corners;
         erase_canvas_once = true;
-    }
-    else if ('w' == event.key) { // balls bounce or wrap around edge
+    } else if (event.key === 'w') { // balls bounce or wrap around edge
         wrap_edge = !wrap_edge;
-    }
-    else if ('e' == event.key) { // erase canvas or leave ball trails
+    } else if (event.key === 'e') { // erase canvas or leave ball trails
         erase_canvas = !erase_canvas;
-    }
-    else if (' ' == event.key) { // pause/resume animation
+    } else if (event.key === ' ') { // pause/resume animation
         if (!cancelAnimation()) {
             request = requestAnimationFrame(animate);
         }
-    }
-    else if ('0' <= event.key && '9' >= event.key) { // specify number of balls
+    } else if (/^[0-9]$/.test(event.key)) { // specify number of balls
         updateCount *= 10;
         updateCount += parseInt(event.key);
-    }
-    else if (event.key === "Enter") { // change number of balls
-        if (0 == updateCount) {
+    } else if (event.key === 'Enter') { // change number of balls
+        if (updateCount === 0) {
             return; // protect against double enter
         }
         cancelAnimation();
@@ -139,19 +127,17 @@ window.addEventListener("keydown", function (event) {
         populate();
         erase_canvas_once = true;
         request = requestAnimationFrame(animate);
-    }
-    else if (event.key === "F1") { // show documentation
+    } else if (event.key === 'F1') { // show documentation
         event.preventDefault();
-        var modal = document.getElementById("documentation-modal");
+        const modal = document.getElementById("documentation-modal");
         if (modal.style.display === "block") {
             modal.style.display = "none";
         } else {
             modal.style.display = "block";
         }
-    }
-    else if (event.key === "Escape") { // hide modal on escape key press
+    } else if (event.key === 'Escape') { // hide modal on escape key press
         event.preventDefault();
-        var modal = document.getElementById("documentation-modal");
+        const modal = document.getElementById("documentation-modal");
         modal.style.display = "none";
     }
 });
@@ -160,15 +146,15 @@ window.onresize = function () {
     cancelAnimation();
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    console.log("canvas.width: " + canvas.width + " " + canvas.height);
+    console.log(`canvas.width: ${canvas.width} ${canvas.height}`);
     populate();
     ctx = canvas.getContext("2d");
     ctx.translate(.5, .5);
     erase_canvas_once = true;
     request = requestAnimationFrame(animate);
-}
+};
 
 window.onload = function () {
     canvas = document.getElementById("my_canvas");
     window.onresize();
-}
+};
